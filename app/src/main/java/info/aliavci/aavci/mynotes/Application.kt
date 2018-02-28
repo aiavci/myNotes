89b8bbb17dd9c40ccd0af135b3eeaa1aa77b034e
Ali Avci <aiavci@yahoo.com>
2018-02-28 00:00:11 -0500

    Unnecessary stetho removed

--- app/src/main/java/info/aliavci/aavci/mynotes/Application.kt
+++ app/src/main/java/info/aliavci/aavci/mynotes/Application.kt
@@ -1,7 +1,6 @@
 package info.aliavci.aavci.mynotes
 
 import android.app.Application
-import com.facebook.stetho.Stetho
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
 
@@ -14,8 +13,5 @@ class Application : Application() {
         super.onCreate()
         // This instantiates DBFlow
         FlowManager.init(FlowConfig.Builder(this).build())
-
-        // Stetho:
-        Stetho.initializeWithDefaults(this)
     }
 }
